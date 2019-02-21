99ade9d37dac66f0698aa1edbfd4f39914c72394
mayanka010 <mayanka010@gmail.com>
2019-02-21 08:27:53 +0530

    add constructor with attr data

--- src/main/SinglyLinkedList.java
+++ src/main/SinglyLinkedList.java
@@ -5,5 +5,9 @@ public class SinglyLinkedList
     private static class Node {
         private int data;
         private Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
     }
 }
